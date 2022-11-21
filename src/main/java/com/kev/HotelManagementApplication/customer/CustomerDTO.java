01c1ac98341aaa4ac37a05cf0376081f3c17b6b4
Kev Carmichael <kev.carmichael22@gmail.com>
2022-11-21 09:35:14 +0000

    created customer/CustomerDTO

---
+++ src/main/java/com/kev/HotelManagementApplication/customer/CustomerDTO.java
@@ -0,0 +1,17 @@
+package com.kev.HotelManagementApplication.customer;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+
+public class CustomerDTO {
+    private final int id;
+    private final String name;
+}
