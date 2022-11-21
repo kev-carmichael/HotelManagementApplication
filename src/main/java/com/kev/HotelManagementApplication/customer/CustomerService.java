17f65e404cab857cbfd7ded0dbd9d822a6938872
Kev Carmichael <kev.carmichael22@gmail.com>
2022-11-21 10:45:56 +0000

    added getCustomerList() to CustomerService

--- src/main/java/com/kev/HotelManagementApplication/customer/CustomerService.java
+++ src/main/java/com/kev/HotelManagementApplication/customer/CustomerService.java
@@ -1,17 +1,27 @@
 package com.kev.HotelManagementApplication.customer;
 
 
+import com.kev.HotelManagementApplication.entity.Customer;
+import com.kev.HotelManagementApplication.factory.DTOFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private DTOFactory dtoFactory;
 
     public List<CustomerDTO> getCustomerList() {
-        return customerRepository.findAll();
+        List<CustomerDTO> list = new ArrayList<>();
+        for (Customer customer : customerRepository.findAll()){
+            CustomerDTO customerDTO = dtoFactory.createDTO(customer);
+            list.add(customerDTO);
+        }
+        return list;
     }
+    
 }
