04f54f93262f327d7fe6aee101602fb95398f9a9
Kev Carmichael <kev.carmichael22@gmail.com>
2022-11-22 13:34:44 +0000

    created room/RoomDTO

---
+++ src/main/java/com/kev/HotelManagementApplication/room/RoomDTO.java
@@ -0,0 +1,17 @@
+package com.kev.HotelManagementApplication.room;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+
+public class RoomDTO {
+    private final int roomId;
+    private final String roomNumber;
+}
