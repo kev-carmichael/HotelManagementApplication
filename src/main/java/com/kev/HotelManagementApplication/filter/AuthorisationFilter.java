9f1e34afa1f20c8ee62d76fcd1f7136993999b5e
Kev Carmichael <kev.carmichael22@gmail.com>
2023-01-17 14:08:03 +0000

    added staff authorized viewallbookings and viewallrooms

--- src/main/java/com/kev/HotelManagementApplication/filter/AuthorisationFilter.java
+++ src/main/java/com/kev/HotelManagementApplication/filter/AuthorisationFilter.java
@@ -49,7 +49,9 @@ public class AuthorisationFilter implements Filter
         Staff staff = staffService.checkCredentials(token);
 
         if(staff != null) {
-            if (requestURI.startsWith("/customer/all")) {
+            if (requestURI.startsWith("/customer/all") ||
+                    requestURI.startsWith("/booking/all") ||
+                    requestURI.startsWith("/room/all")){
                 System.out.println("STAFF NOT NULL FOR /customer/all");
                 return true;
             } else if (requestURI.startsWith("/staff/logout/")) {
